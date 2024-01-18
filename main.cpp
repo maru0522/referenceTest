6c57f0fd5935990e50c09893d4f50f1caed4addd
maru0522 <94513876+maru0522@users.noreply.github.com>
2024-01-18 16:56:06 +0900

    end

--- main.cpp
+++ main.cpp
@@ -1,33 +1,42 @@
-#include <iostream>
 #include <DirectXMath.h>
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
-// stdmoveの仕様について知る
-
-struct A
-{
-    A() {}
-
-    // 左辺値からコピー
-    A(const A&)
-    {
-        std::cout << "copy" << std::endl;
-    }
-
-    // 右辺値からムーブ
-    A(A&&)
-    {
-        std::cout << "move" << std::endl;
-    }
-};
+// std::ifstreamでテスト
 
 int main()
 {
-    std::unique_ptr<A> a1 = nullptr;
-    if (a1.get() == nullptr) { std::cout << "a1.get() == nullptr"; }
-    if (a1 == nullptr) { std::cout << "a1 == nullptr"; }
+    //std::ifstream ifs(str);
+    std::istringstream strs("AAABBBCCCDDDEEE"); // istringstreamをifstreamに見立ててる。
+    std::istringstream strs2("AAA,BBB,CCC,DDD,EEE"); // istringstreamをifstreamに見立ててる。
+
+    //ifs.seekg(0, std::ios_base::beg);
+    strs.seekg(3, std::ios_base::beg);
+
+    std::string out;
+    strs >> out;
+    std::cout << out << std::endl;
+
+
+    // ,は含まれるのか？
+    std::string str;
+    while (std::getline(strs2, str))
+    {
+        std::string word;
+        std::istringstream iss(str);
+        while (std::getline(iss, word, ','))
+        {
+            std::cout << word << std::endl;
+        }
+    }
+
+    std::string a = { "12345" };
+    std::cout << a.size() << std::endl;
+    std::cout << a[a.size()];
 
     return 0;
 }
